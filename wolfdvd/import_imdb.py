cd61c0733f27fb6b2f3e26c52759470d8ad9721b
Henry Lambert <henry.lambert@materials.ox.ac.uk>
2015-04-12 19:52:27 +0100

    should add imdbid to the movie objects then every view will become title, wolfloc, director, and can link to imdb page.

--- wolfdvd/import_imdb.py
+++ wolfdvd/import_imdb.py
@@ -16,35 +16,36 @@ def imdb_to_dict():
   tits_list_dict = []
   for i, tit in enumerate(tits):
       title ={}
-      print i+1,'/', len(tits),tit.name
+      print i+1,'/', len(tits), tit.name
       try:
           movie = ia.get_movie('%s'%tit.imdbid)
-          title['title'] = movie['title']
-          title['wolfloc'] = tit.wolfloc
-          title['year']    = movie['year']
+          title['title']    = movie['title']
+          title['wolfloc']  = tit.wolfloc
+          title['year']     = movie['year']
           title['director'] = movie['director'][0]['name']
           title['country']  =  movie['countries'][0]
+          title['imdbid']   =  tit.imdbid
       except:
           print tit.name
           print 'no imdb id number', tit.imdbid
-          title['title'] = tit.name
-          title['wolfloc'] = tit.wolfloc
-        
-      tits_list_dict.append(title)
+          title['title']    = tit.name
+          title['wolfloc']  = tit.wolfloc
+          title['imdbid']   =  0
 
+      tits_list_dict.append(title)
   for tit in tits_list_dict:
       print tit.values()
   return tits_list_dict
 
 def dump_updated_db(tits_list):
-  f = open('tits_list_dict.pckl','w')
+  f = open('tits_list_dict1.pckl','w')
   pickle.dump(tits_list, f)
   f.close()
   
 if __name__=='__main__':
-  db = sys.argv[1]
-  tits_list = load_titles(db)
+  #db = sys.argv[1]
+  tits = load_titles()
+  tits_list = imdb_to_dict(tits[1:5])
+  #tits_list = load_titles(db)
   dump_updated_db(tits_list)
    
-
-
